4007b4bf552df580e29a4d67e9c36024bcaec011
Dana Munteanu <dana.maria.munteanu@gmail.com>
2021-03-24 12:25:50 +0200

    Added Header & Footer layout

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,15 @@
 import './Footer.scss';
+import { ReactComponent as TwitterLogo} from './../../twitter.svg';
+import { ReactComponent as FacebookLogo} from './../../facebook.svg';
 
 function Footer() {
   return (
-    <footer>
+    <footer className="footer">
+      <div className="footer__copyright">Copyright © CSS Press 2021</div>
+      <div className="footer__social">
+        <TwitterLogo className="footer__social--twitter"/>
+        <FacebookLogo className="footer__social--facebook"/>
+      </div>
     </footer>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,14 @@ import './Header.scss';
 
 function Header() {
   return (
-    <header>
+    <header className="header">
+      <div className="header__name">CSS Press</div>
+      <div className="header__lines">
+        <div className="header__lines--25"></div>
+        <div className="header__lines--50"></div>
+        <div className="header__lines--100"></div>
+      </div>
+      <div className="header__user"></div>
     </header>
   );
 }

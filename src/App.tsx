5fca68c46ad081534d27461cd66f8bfafa0da86b
Bogdan Mihai Nicolae <bmnicolae@gmail.com>
2022-04-16 23:10:51 +0300

    Notfound Page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import GamePage from './pages/game-page/GamePage';
+import NotfoundPage from './pages/notfound-page/NotfoundPage';
 
 import './App.scss';
 
@@ -15,6 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/chapter/1/level/1" />}></Route>
           <Route path="/chapter/:chapterId/level/:levelId" element={<GamePage/>}/>
+          <Route path="/not-found" element={<NotfoundPage/>}/>
+          <Route path="*" element={<Navigate replace to="/not-found" />}/>
         </Routes>
         <Footer/>
       </div>

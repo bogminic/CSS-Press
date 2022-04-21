18a21b437dcc635fe5c6ddf46274f685fc2a7bcf
Bogdan Mihai Nicolae <bmnicolae@gmail.com>
2022-04-21 21:34:18 +0300

    Added tooltips for instructions

---
+++ src/const/tooltips.ts
@@ -0,0 +1,4 @@
+export const tooltips: { [key: string]: string } = {
+    'font-family': '<p>Specifies the font and its fallbacks, separated by comma.\n <code>specific-font-family</code> | <code>generic-font-family</code> | <code>initial</code> | <code>inherit</code>\n font-family: "Crimson Text", serif;</p>',
+    'font-size': '<p>Defines the size of your text.\n <code>number-of-units unit</code>\n font-size: 3.5em;',
+}

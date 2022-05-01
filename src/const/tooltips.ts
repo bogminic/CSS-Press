a1987f82e4bc2da1ce18552fed02720507ffcdd6
Bogdan Mihai Nicolae <bmnicolae@gmail.com>
2022-05-01 08:40:04 +0300

    Level font-style

--- src/const/tooltips.ts
+++ src/const/tooltips.ts
@@ -1,4 +1,5 @@
 export const tooltips: { [key: string]: string } = {
     'font-family': '<p>Specifies the font and its fallbacks, separated by comma.\n <code>specific-font-family</code> | <code>generic-font-family</code> | <code>initial</code> | <code>inherit</code>\n font-family: "Crimson Text", serif;</p>',
     'font-size': '<p>Defines the size of your text.\n <code>number-of-units unit</code>\n font-size: 3.5em;',
+    'font-style': '<p>Names the style to be applied to the text\n <code>normal</code> | <code>italic</code> | <code>oblique</code> | <code>initial</code> | <code>inherit</code> \n font-style: normal;',
 }
